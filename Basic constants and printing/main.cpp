d18652bf66397e3d45ffe7de3bc309398e9666d2
yunu0512 <111822797+yunu0512@users.noreply.github.com>
2025-09-15 16:02:34 +0900

    123

---
+++ Basic constants and printing/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("===Basic constants=== \n");
+    printf("name   :%s\n","taejun");
+    printf("initial: %c\n", 's');
+    printf("Age: %d\n", 21);
+    printf("score  : %6f\n", 93.5f);
+    printf("count  : %u\n", 3000000000);
+
+    return 0;
+}
